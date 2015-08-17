545bed489bd389b6d918909abb8501c86373bac6
qqkk <qqkk@qqkkdeiMac.local>
2015-08-17 22:28:42 +0800

    b-home indented block  problem

--- rango/views.py
+++ rango/views.py
@@ -5,38 +5,25 @@ from rango.models import Page
 from django.http import HttpResponse
 
 def index(request):
-	category_list = Category.objects.order_by('likes')[:5]
-	context_dict={'boldmessage':"i am bold font from the context",'categories':category_list}
+	category_list = Category.objects.order_by('-likes')[:5]
+	context_dict = {'boldmessage':"i am bold font from the context",'categories':category_list}
 	#return HttpResponse("Rango says: Hello world! <br/> <a href='/rango/about'>About</a>")
 	return render(request,'rango/index.html',context_dict)
 	pass
 
 def about(request):
-	return HttpResponse("Rango says here is the about page<br/> <a href='/rango/'>index</a>")
-	pass	
+    return HttpResponse("Rango says here is the about page<br/> <a href='/rango/'>index</a>")
+    pass
 
-def category(request, category_name_slug):
-	context_dict = {}
-	try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
+def category(request,category_name_slug):
+    context_dict = {}
+    try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
-
-        # Retrieve all of the associated pages.
-        # Note that filter returns >= 1 model instance.
         pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
-    except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
         pass
-
-    # Go render the response and return it to the client.
-    return render(request, 'rango/category.html', context_dict)
+    except Category.DoesNotExist:
+        pass
+    return render(request,'rango/category.html',context_dict)
